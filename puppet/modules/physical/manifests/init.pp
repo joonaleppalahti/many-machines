bf3f8daf7c002a7029e76d36567d403bdb31e2e5
Joona Leppälahti <joona.leppalahti@myy.haaga-helia.fi>
2016-12-02 22:26:33 +0200

    physical machines run vagrant

--- puppet/modules/physical/manifests/init.pp
+++ puppet/modules/physical/manifests/init.pp
@@ -1,8 +1,22 @@
 class physical {
-	file { "/tmp/physicaljuttu":
-		content => "Hello Physical World \n"
+
+	file { "/etc/vagrantinstall":
+		ensure => "directory"
 	}
 
-	
+	file { "/etc/vagrantinstall/Vagrantfile":
+		content => template("physical/Vagrantfile"),
+	}
+
+	file { "/etc/vagrantinstall/provision.sh":
+		content => template("physical/provision.sh"),
+	}
+
+	exec { "vagrant up":
+		timeout => 0,
+		command => "sudo vagrant up",
+		path => "/bin/:/usr/bin/:/sbin/:/usr/sbin/",
+		cwd => "/etc/vagrantinstall"
+	}
 
 }
