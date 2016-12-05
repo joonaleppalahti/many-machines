a427f699349758e7a92386cd9cca9c09db1eb993
joonaleppalahti <joona.leppalahti@myy.haaga-helia.fi>
2016-12-05 14:36:00 +0000

    multiple clients in dhcpd.conf

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,4 @@
-sudo tftp 192.168.1.129 -c get firstboot
+sudo tftp 172.28.172.136 -c get firstboot
 sudo mv firstboot /etc/init.d/
 sudo chmod +x /etc/init.d/firstboot
 update-rc.d firstboot defaults
